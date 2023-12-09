75a41ae1124bece92ad7cb0c9ff0dd236e46fb7e
Darkblazer21 <128042259+Darkblazer21@users.noreply.github.com>
2023-12-09 23:49:09 +0000

    Update session_state in both pages

--- pages/1_first_page.py
+++ pages/1_first_page.py
@@ -8,27 +8,35 @@ st.sidebar.markdown("# C'est la première page 🌟")
 
 st.markdown("## Volume de trafic Internet & VPN généré par une \"Data Card\" en UL/DL et DL")
 
-data = pd.DataFrame(columns=['Type de services', 'Nombre de session à l\'HC', 'Taille session à l\'HC', 'Pourcentage DL', 'Volume trafic à l\'HC en UL/DL (Mb)', 'Volume trafic à l\'HC en DL (Mb)'])
+# Initialiser l'état de la session avec un dataframe vide
+# Initialiser l'état de la session avec un dataframe vide
+if "data_page1" not in st.session_state:
+    st.session_state.data_page1 = pd.DataFrame(columns=['Type de services', 'Nombre de session à l\'HC', 'Taille session à l\'HC', 'Pourcentage DL', 'Volume trafic à l\'HC en UL/DL (Mb)', 'Volume trafic à l\'HC en DL (Mb)'])
+
 
 services = ['Navigation web', 'Email', 'Streaming video', 'VPN', 'Gaming']
-for i, service in enumerate(services):
-    data.loc[i] = [service, None, None, None, None, None]
+
+if st.session_state.data_page1.empty:
+    for i, service in enumerate(services):
+        st.session_state.data_page1.loc[i] = [service, None, None, None, None, None]
 
 with st.form(key='my_form'):
-    for index, row in data.iterrows():
+    for index, row in st.session_state.data_page1.iterrows():
         st.write(row['Type de services'])
-        session = st.number_input('Nombre de session à l\'HC', key=f"session_{index}")
-        taille = st.number_input('Taille session à l\'HC', key=f"taille_{index}")
-        pourcentage = st.number_input('Pourcentage DL', min_value=0.0, max_value=100.0, key=f"pourcentage_{index}")
-        data.at[index, 'Nombre de session à l\'HC'] = session
-        data.at[index, 'Taille session à l\'HC'] = taille
-        data.at[index, 'Pourcentage DL'] = pourcentage
+
+        session = st.number_input('Nombre de session à l\'HC', key=f"session_{index}", value=row['Nombre de session à l\'HC'] or 0.0)
+        taille = st.number_input('Taille session à l\'HC', key=f"taille_{index}", value=row['Taille session à l\'HC'] or 0.0)
+        pourcentage = st.number_input('Pourcentage DL', min_value=0.0, max_value=100.0, key=f"pourcentage_{index}", value=row['Pourcentage DL'] or 0.0)
+
+        st.session_state.data_page1.at[index, 'Nombre de session à l\'HC'] = session
+        st.session_state.data_page1.at[index, 'Taille session à l\'HC'] = taille
+        st.session_state.data_page1.at[index, 'Pourcentage DL'] = pourcentage
         st.write('---')
 
     submit_button = st.form_submit_button(label='Calculer')
 
 if submit_button:
-    data['Volume trafic à l\'HC en UL/DL (Mb)'] = data['Taille session à l\'HC'] * data['Nombre de session à l\'HC']
-    data['Volume trafic à l\'HC en DL (Mb)'] = data['Volume trafic à l\'HC en UL/DL (Mb)'] * (data['Pourcentage DL'] / 100)
+    st.session_state.data_page1['Volume trafic à l\'HC en UL/DL (Mb)'] = st.session_state.data_page1['Taille session à l\'HC'] * st.session_state.data_page1['Nombre de session à l\'HC']
+    st.session_state.data_page1['Volume trafic à l\'HC en DL (Mb)'] = st.session_state.data_page1['Volume trafic à l\'HC en UL/DL (Mb)'] * (st.session_state.data_page1['Pourcentage DL'] / 100)
 
-st.table(data)
+st.table(st.session_state.data_page1)
--- pages/2_second_page.py
+++ pages/2_second_page.py
@@ -8,27 +8,31 @@ st.sidebar.markdown("# C'est la deuxième page ✨")
 
 st.markdown("## Volume de trafic Internet & VPN généré par un \"Smartphone\" en UL/DL et DL")
 
-data = pd.DataFrame(columns=['Type de services', 'Nombre de session à l\'HC', 'Taille session à l\'HC', 'Pourcentage DL', 'Volume trafic à l\'HC en UL/DL (Mb)', 'Volume trafic à l\'HC en DL (Mb)'])
+# Initialiser l'état de la session avec un dataframe vide
+if "data_page2" not in st.session_state:
+    st.session_state.data_page2 = pd.DataFrame(columns=['Type de services', 'Nombre de session à l\'HC', 'Taille session à l\'HC', 'Pourcentage DL', 'Volume trafic à l\'HC en UL/DL (Mb)', 'Volume trafic à l\'HC en DL (Mb)'])
 
 services = ['Navigation web', 'Email', 'Streaming video', 'VPN', 'Gaming']
-for i, service in enumerate(services):
-    data.loc[i] = [service, None, None, None, None, None]
+
+if st.session_state.data_page2.empty:
+    for i, service in enumerate(services):
+        st.session_state.data_page2.loc[i] = [service, None, None, None, None, None]
 
 with st.form(key='my_form'):
-    for index, row in data.iterrows():
+    for index, row in st.session_state.data_page2.iterrows():
         st.write(row['Type de services'])
-        session = st.number_input('Nombre de session à l\'HC', key=f"session_{index}")
-        taille = st.number_input('Taille session à l\'HC', key=f"taille_{index}")
-        pourcentage = st.number_input('Pourcentage DL', min_value=0.0, max_value=100.0, key=f"pourcentage_{index}")
-        data.at[index, 'Nombre de session à l\'HC'] = session
-        data.at[index, 'Taille session à l\'HC'] = taille
-        data.at[index, 'Pourcentage DL'] = pourcentage
+        session = st.number_input('Nombre de session à l\'HC', key=f"session_{index}", value=row['Nombre de session à l\'HC'] or 0.0)
+        taille = st.number_input('Taille session à l\'HC', key=f"taille_{index}", value=row['Taille session à l\'HC'] or 0.0)
+        pourcentage = st.number_input('Pourcentage DL', min_value=0.0, max_value=100.0, key=f"pourcentage_{index}", value=row['Pourcentage DL'] or 0.0)
+        st.session_state.data_page2.at[index, 'Nombre de session à l\'HC'] = session
+        st.session_state.data_page2.at[index, 'Taille session à l\'HC'] = taille
+        st.session_state.data_page2.at[index, 'Pourcentage DL'] = pourcentage
         st.write('---')
 
     submit_button = st.form_submit_button(label='Calculer')
 
 if submit_button:
-    data['Volume trafic à l\'HC en UL/DL (Mb)'] = data['Taille session à l\'HC'] * data['Nombre de session à l\'HC']
-    data['Volume trafic à l\'HC en DL (Mb)'] = data['Volume trafic à l\'HC en UL/DL (Mb)'] * (data['Pourcentage DL'] / 100)
+    st.session_state.data_page2['Volume trafic à l\'HC en UL/DL (Mb)'] = st.session_state.data_page2['Taille session à l\'HC'] * st.session_state.data_page2['Nombre de session à l\'HC']
+    st.session_state.data_page2['Volume trafic à l\'HC en DL (Mb)'] = st.session_state.data_page2['Volume trafic à l\'HC en UL/DL (Mb)'] * (st.session_state.data_page2['Pourcentage DL'] / 100)
 
-st.table(data)
+st.table(st.session_state.data_page2)
