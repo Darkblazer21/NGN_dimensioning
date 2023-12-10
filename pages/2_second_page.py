91605f9240d857c89c1770f899027d62c86c42c0
Darkblazer21 <128042259+Darkblazer21@users.noreply.github.com>
2023-12-10 12:18:40 +0000

    Add Total Internet traffic volume in second page

--- pages/2_second_page.py
+++ pages/2_second_page.py
@@ -12,6 +12,19 @@ st.markdown("## Volume de trafic Internet & VPN généré par un \"Smartphone\" 
 if "data_page2" not in st.session_state:
     st.session_state.data_page2 = pd.DataFrame(columns=['Type de services', 'Nombre de session à l\'HC', 'Taille session à l\'HC', 'Pourcentage DL', 'Volume trafic à l\'HC en UL/DL (Mb)', 'Volume trafic à l\'HC en DL (Mb)'])
 
+if "total_internet_ul_dl_phone" not in st.session_state:
+    st.session_state.total_internet_ul_dl_phone = 0.0
+
+if "total_internet_dl_phone" not in st.session_state:
+    st.session_state.total_internet_dl_phone = 0.0
+
+# if "total_vpn_ul_dl_phone" not in st.session_state:
+#     st.session_state.total_vpn_ul_dl_phone = 0.0
+
+# if "total_vpn_dl_phone" not in st.session_state:
+#     st.session_state.total_vpn_dl_phone = 0.0
+
+
 services = ['Navigation web', 'Email', 'Streaming video', 'VPN', 'Gaming']
 
 if st.session_state.data_page2.empty:
@@ -32,7 +45,38 @@ with st.form(key='my_form'):
     submit_button = st.form_submit_button(label='Calculer')
 
 if submit_button:
+    # Réinitialisez les valeurs supplémentaires à zéro
+    st.session_state.total_internet_ul_dl_phone = 0.0
+    st.session_state.total_internet_dl_phone = 0.0
+    # st.session_state.total_vpn_ul_dl_phone = 0.0
+    # st.session_state.total_vpn_dl_phone = 0.0
+
+    for index, row in st.session_state.data_page2.iterrows():
+        volume_ul_dl_phone = row['Volume trafic à l\'HC en UL/DL (Mb)']
+        volume_dl_phone = row['Volume trafic à l\'HC en DL (Mb)']
+        service = row['Type de services']
+
+        if service != 'VPN':
+            if volume_ul_dl_phone is not None:
+                st.session_state.total_internet_ul_dl_phone += volume_ul_dl_phone
+            if volume_dl_phone is not None:
+                st.session_state.total_internet_dl_phone += volume_dl_phone
+        # else:
+        #     if volume_ul_dl_phone is not None:
+        #         st.session_state.total_vpn_ul_dl_phone += volume_ul_dl_phone
+        #     if volume_dl_phone is not None:
+        #         st.session_state.total_vpn_dl_phone += volume_dl_phone  
+
     st.session_state.data_page2['Volume trafic à l\'HC en UL/DL (Mb)'] = st.session_state.data_page2['Taille session à l\'HC'] * st.session_state.data_page2['Nombre de session à l\'HC']
     st.session_state.data_page2['Volume trafic à l\'HC en DL (Mb)'] = st.session_state.data_page2['Volume trafic à l\'HC en UL/DL (Mb)'] * (st.session_state.data_page2['Pourcentage DL'] / 100)
 
 st.table(st.session_state.data_page2)
+
+# Affichage des valeurs supplémentaires
+st.markdown("## Volume total du trafic Internet")
+st.write(f"- UL/DL : {st.session_state.total_internet_ul_dl_phone} Mb")
+st.write(f"- DL : {st.session_state.total_internet_dl_phone} Mb")
+
+# st.markdown("## Volume total du trafic VPN")
+# st.write(f"- UL/DL : {st.session_state.total_vpn_ul_dl_phone} Mb")
+# st.write(f"- DL : {st.session_state.total_vpn_dl_phone} Mb")
