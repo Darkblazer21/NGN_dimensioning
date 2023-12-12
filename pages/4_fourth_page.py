828947c963c24d2ed23d787fe27f7ab8efc1bf63
Darkblazer21 <128042259+Darkblazer21@users.noreply.github.com>
2023-12-12 21:59:38 +0000

    Create fourth page

---
+++ pages/4_fourth_page.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Plan Usager EPC", page_icon="📊", layout="centered")
+
+st.markdown("# Dimensionnement du plan usager de l'EPC 📊")
+st.sidebar.markdown("# C'est la quatrième page 🌌")
+
+st.markdown("## Dimensionner le plan usager de l'EPC (données des utilisateurs Smarphones + Data Card)")
