e7a28547be8e404ae7becb70344f59015e51d18f
Darkblazer21 <128042259+Darkblazer21@users.noreply.github.com>
2023-12-13 01:41:14 +0000

    Update code in page 3

--- pages/3_third_page.py
+++ pages/3_third_page.py
@@ -45,6 +45,49 @@ if "total_vpn_dl_card" not in st.session_state:
 if "vpn_via_data_card" not in st.session_state:
     st.session_state.vpn_via_data_card = 0.0
 
+# Etat de session des variables résultantes de calculs :
+if "volume_trafic_total_phone" not in st.session_state:
+    st.session_state.volume_trafic_total_phone = 0.0
+
+if "volume_trafic_total_card" not in st.session_state:
+    st.session_state.volume_trafic_total_card = 0.0
+
+if "volume_trafic_total" not in st.session_state:
+    st.session_state.volume_trafic_total = 0.0
+
+if "debit_internet" not in st.session_state:
+    st.session_state.debit_internet = 0.0
+
+if "data_card_using_vpn" not in st.session_state:
+    st.session_state.data_card_using_vpn = 0.0
+
+if "trafic_total_vpn" not in st.session_state:
+    st.session_state.trafic_total_vpn = 0.0
+
+if "debit_total_vpn" not in st.session_state:
+    st.session_state.debit_total_vpn = 0.0
+
+if "volume_trafic_total_phone_dl" not in st.session_state:
+    st.session_state.volume_trafic_total_phone_dl = 0.0
+
+if "volume_trafic_total_card_dl" not in st.session_state:
+    st.session_state.volume_trafic_total_card_dl = 0.0
+
+if "volume_trafic_total_dl" not in st.session_state:
+    st.session_state.volume_trafic_total_dl = 0.0
+
+if "debit_internet_dl" not in st.session_state:
+    st.session_state.debit_internet_dl = 0.0
+
+if "data_card_using_vpn_dl" not in st.session_state:
+    st.session_state.data_card_using_vpn_dl = 0.0
+
+if "trafic_total_vpn_dl" not in st.session_state:
+    st.session_state.trafic_total_vpn_dl = 0.0
+
+if "debit_total_vpn_dl" not in st.session_state:
+    st.session_state.debit_total_vpn_dl = 0.0
+
 total_subscribers = st.number_input("Nombre total d'abonnés", min_value=0.0, step=1.0, value=st.session_state.total_subscribers)
 percentage_data_card = st.number_input("Pourcentage Data Card", min_value=0.0, max_value=100.0, value=st.session_state.percentage_data_card)
 percentage_smartphone = st.number_input("Pourcentage Smartphone", min_value=0.0, max_value=100.0, value=st.session_state.percentage_smartphone)
@@ -55,10 +98,12 @@ st.session_state.percentage_smartphone = percentage_smartphone
 
 num_data_card = int(total_subscribers * (percentage_data_card / 100)) if percentage_data_card is not None else None
 num_smartphone = int(total_subscribers * (percentage_smartphone / 100)) if percentage_smartphone is not None else None
+vpn_via_data_card = st.number_input("VPN (via Data Card) en pourcentage", min_value=0.0, max_value=100.0, value=st.session_state.vpn_via_data_card)
 
 total_subscribers = int(total_subscribers)
 st.session_state.num_data_card = num_data_card
 st.session_state.num_smartphone = num_smartphone
+st.session_state.vpn_via_data_card = vpn_via_data_card
 
 data = {
     "Nombre total d'abonnés": [total_subscribers],
@@ -68,46 +113,55 @@ data = {
 df = pd.DataFrame(data)
 st.table(df)
 
-st.markdown(" **:blue[Procédons au calcul maintenant, remplis les champs suivants :]**")
+st.markdown(" **:blue[Procédons au calcul maintenant, Appuyez sur le bouton calculer :]**")
 
-col1, col2 = st.columns(2)
+# col1, col2 = st.columns(2)
 
-with col1:
-    # Demander à l'utilisateur de saisir les valeurs
-    total_internet_ul_dl_phone = st.number_input("Total trafic Internet en UL/DL pour les Smartphones", value=st.session_state.total_internet_ul_dl_phone)
-    total_internet_ul_dl_card = st.number_input("Total trafic Internet en UL/DL pour les Data cards", value=st.session_state.total_internet_ul_dl_card)
-    total_vpn_ul_dl_card = st.number_input("Total trafic VPN en UL/DL pour les Data Card", value=st.session_state.total_vpn_ul_dl_card)
-    vpn_via_data_card = st.number_input("VPN (via Data Card) en pourcentage", min_value=0.0, max_value=100.0, value=st.session_state.vpn_via_data_card)
+# with col1:
+#     # Demander à l'utilisateur de saisir les valeurs
+#     total_internet_ul_dl_phone = st.number_input("Total trafic Internet en UL/DL pour les Smartphones", value=st.session_state.total_internet_ul_dl_phone)
+#     total_internet_ul_dl_card = st.number_input("Total trafic Internet en UL/DL pour les Data cards", value=st.session_state.total_internet_ul_dl_card)
+#     total_vpn_ul_dl_card = st.number_input("Total trafic VPN en UL/DL pour les Data Card", value=st.session_state.total_vpn_ul_dl_card)
+#     vpn_via_data_card = st.number_input("VPN (via Data Card) en pourcentage", min_value=0.0, max_value=100.0, value=st.session_state.vpn_via_data_card)
 
-with col2:
-    total_internet_dl_phone = st.number_input("Total trafic Internet en DL pour les Smartphones", value=st.session_state.total_internet_dl_phone)
-    total_internet_dl_card = st.number_input("Total trafic Internet en DL pour les Data cards", value=st.session_state.total_internet_dl_card)
-    total_vpn_dl_card = st.number_input("Total trafic VPN en DL pour les Data Card", value=st.session_state.total_vpn_dl_card)
+# with col2:
+#     total_internet_dl_phone = st.number_input("Total trafic Internet en DL pour les Smartphones", value=st.session_state.total_internet_dl_phone)
+#     total_internet_dl_card = st.number_input("Total trafic Internet en DL pour les Data cards", value=st.session_state.total_internet_dl_card)
+#     total_vpn_dl_card = st.number_input("Total trafic VPN en DL pour les Data Card", value=st.session_state.total_vpn_dl_card)
 
-st.session_state.total_internet_ul_dl_phone = total_internet_ul_dl_phone
-st.session_state.total_internet_ul_dl_card = total_internet_ul_dl_card
-st.session_state.total_vpn_ul_dl_card = total_vpn_ul_dl_card
-st.session_state.vpn_via_data_card = vpn_via_data_card
+# st.session_state.total_internet_ul_dl_phone = total_internet_ul_dl_phone
+# st.session_state.total_internet_ul_dl_card = total_internet_ul_dl_card
+# st.session_state.total_vpn_ul_dl_card = total_vpn_ul_dl_card
+# st.session_state.vpn_via_data_card = vpn_via_data_card
 
-st.session_state.total_internet_dl_phone = total_internet_dl_phone
-st.session_state.total_internet_dl_card = total_internet_dl_card
-st.session_state.total_vpn_dl_card = total_vpn_dl_card
+# st.session_state.total_internet_dl_phone = total_internet_dl_phone
+# st.session_state.total_internet_dl_card = total_internet_dl_card
+# st.session_state.total_vpn_dl_card = total_vpn_dl_card
 
-volume_trafic_total_phone = 0.0
-volume_trafic_total_card = 0.0
-volume_trafic_total = 0.0
-debit_internet = 0.0
-data_card_using_vpn = 0.0
-trafic_total_vpn = 0.0
-debit_total_vpn = 0.0
+total_internet_ul_dl_phone = st.session_state.total_internet_ul_dl_phone 
+total_internet_ul_dl_card = st.session_state.total_internet_ul_dl_card 
+total_vpn_ul_dl_card = st.session_state.total_vpn_ul_dl_card 
+vpn_via_data_card = st.session_state.vpn_via_data_card 
 
-volume_trafic_total_phone_dl = 0.0
-volume_trafic_total_card_dl = 0.0
-volume_trafic_total_dl = 0.0
-debit_internet_dl = 0.0
-data_card_using_vpn_dl = 0.0
-trafic_total_vpn_dl = 0.0
-debit_total_vpn_dl = 0.0
+total_internet_dl_phone = st.session_state.total_internet_dl_phone
+total_internet_dl_card = st.session_state.total_internet_dl_card
+total_vpn_dl_card = st.session_state.total_vpn_dl_card 
+
+# Création des variables aux résultantes de calculs
+volume_trafic_total_phone = st.session_state.volume_trafic_total_phone
+volume_trafic_total_card = st.session_state.volume_trafic_total_card
+volume_trafic_total = st.session_state.volume_trafic_total
+debit_internet = st.session_state.debit_internet
+data_card_using_vpn = st.session_state.data_card_using_vpn
+trafic_total_vpn = st.session_state.trafic_total_vpn
+debit_total_vpn = st.session_state.debit_total_vpn
+volume_trafic_total_phone_dl = st.session_state.volume_trafic_total_phone_dl
+volume_trafic_total_card_dl = st.session_state.volume_trafic_total_card_dl
+volume_trafic_total_dl = st.session_state.volume_trafic_total_dl
+debit_internet_dl = st.session_state.debit_internet_dl
+data_card_using_vpn_dl = st.session_state.data_card_using_vpn_dl
+trafic_total_vpn_dl = st.session_state.trafic_total_vpn_dl
+debit_total_vpn_dl = st.session_state.debit_total_vpn_dl
 
 if st.button("Calculer"):
     # UL/DL
@@ -129,6 +183,26 @@ if st.button("Calculer"):
     debit_total_vpn_dl = trafic_total_vpn_dl * 8 * 1000 / 3600
 
 
+
+# Mise à jour des valeurs dans l'état de session :
+st.session_state.volume_trafic_total_phone = volume_trafic_total_phone
+st.session_state.volume_trafic_total_card = volume_trafic_total_card
+st.session_state.volume_trafic_total = volume_trafic_total
+st.session_state.debit_internet = debit_internet
+st.session_state.data_card_using_vpn = data_card_using_vpn
+st.session_state.trafic_total_vpn = trafic_total_vpn
+st.session_state.debit_total_vpn = debit_total_vpn
+st.session_state.volume_trafic_total_phone_dl = volume_trafic_total_phone_dl
+st.session_state.volume_trafic_total_card_dl = volume_trafic_total_card_dl
+st.session_state.volume_trafic_total_dl = volume_trafic_total_dl
+st.session_state.debit_internet_dl = debit_internet_dl
+st.session_state.data_card_using_vpn_dl = data_card_using_vpn_dl
+st.session_state.trafic_total_vpn_dl = trafic_total_vpn_dl
+st.session_state.debit_total_vpn_dl = debit_total_vpn_dl
+
+
+
+
 # Afficher le tableau des résultats
 data2 = {
     "Désignation": [
@@ -140,7 +214,7 @@ data2 = {
         "Trafic total pour VPN (Gb)",
         "Débit total VPN (data card) (Gbits/s)"
     ],
-    "Trafic total à l'HC - DL/HL": [
+    "Trafic total à l'HC - DL/UL": [
         volume_trafic_total_phone,
         volume_trafic_total_card,
         volume_trafic_total,
@@ -180,3 +254,4 @@ df3 = pd.DataFrame(data3)
 col1, col2 = st.columns(2)
 col1.table(df2)
 col2.table(df3)
+
